c802902385354ad6c690b14f38e9efe6ad66d9cf
Elvira <sabirova.elvira.gam@gmail.com>
2022-09-11 10:56:18 +0500

    feat: add user controller

---
+++ controllers/user.js
@@ -0,0 +1,40 @@
+const User = require('../models/user');
+const {
+  NotFoundError,
+} = require('../utils/errors/NotFoundError');
+const { BadRequest } = require('../utils/errors/BadRequestError');
+const { Unauthorized } = require('../utils/errors/UnauthorizedError');
+const { ConflictError } = require('../utils/errors/ConflictError');
+
+
+module.exports.getUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .orFail(new NotFoundError('Нет пользователя с таким id'))
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch(next);
+};
+
+module.exports.updateUser = (req, res, next) => {
+  const {
+    name,
+    email,
+  } = req.body;
+  User.findByIdAndUpdate(req.user._id, {
+    name,
+    email,
+  }, {
+    runValidators: true,
+    new: true,
+  })
+    .orFail(new NotFoundError('Нет пользователя с таким id'))
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequest('Некорректные данные при обновлении пользователя'));
+      } else {
+        next(err);
+      }
+    });
+};
